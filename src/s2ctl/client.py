8d9aa62f84c0baa32641f58a229894b631a47f66
Ivchenkov, Sergei <sergei.ivchenkov@vstack.com>
2024-08-06 11:18:55 +0300

    dev-7565 add uzum endpoint

--- src/s2ctl/client.py
+++ src/s2ctl/client.py
@@ -25,6 +25,7 @@ HOSTS_MAP = types.MappingProxyType({
     '25': 'https://api.vc.miran.ru',
     '26': 'https://api.ekacod.ru',
     '28': 'https://api.glos.online',
+    '29': 'https://api.vcloud-test.uzum.io',
 })
 
 
